60183668573212ce29ec1ba1ceaf2fdc2675c301
911-Budurlean-Andrei <budurlean.andrei@yahoo.com>
2022-10-20 08:55:54 +0300

    Finished lab2

---
+++ Lab2/main.py
@@ -0,0 +1,67 @@
+class SymbolTable:
+
+    def __init__(self, size = 4093):
+        self._array = [None] * size
+        self._size = size
+
+    def hash(self, value):
+        if type(value) is str:
+            sum = 0
+            for character in value:
+                sum = (sum + ord(character)) % self._size
+            return sum
+        if type(value) is int:
+            return value % self._size
+
+    def add(self, value):
+        key = self.hash(value)
+        if self._array[key] is not None:
+            for elem in self._array[key]:
+                if elem is key:
+                    return
+                else:
+                    self._array[key].append(value)
+
+        else:
+            self._array[key] = []
+            self._array[key].append(value)
+
+    def get(self, value):
+        key = self.hash(value)
+
+        if self._array[key] is None:
+            return None, None
+
+        for elem in self._array[key]:
+            if elem is value:
+                return key, self._array[key].index(elem)
+
+        return None, None
+
+    def hasValue(self, value):
+        key = self.hash(value)
+
+        if self._array[key] is None:
+            return False
+
+        for elem in self._array[key]:
+            if elem is value:
+                return True
+
+        return False
+
+
+
+table = SymbolTable()
+
+table.add("abc")
+table.add(110)
+table.add("Table")
+
+assert(table.hasValue(110) is True)
+assert(table.hasValue("110") is False)
+assert(table.hasValue("idk") is False)
+
+assert(table.get(110) == (110, 0))
+key, pos = table.get("Table")
+assert(table._array[key][pos] == "Table")
